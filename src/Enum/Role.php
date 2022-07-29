d0976421421b4c968212f4ff742a887c7590e4c7
Greg Dev <gvandevray@numericable.fr>
2022-07-29 10:55:30 +0200

    Create blog models' entities

---
+++ src/Enum/Role.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Enum;
+
+enum Role
+{
+    public const ADMIN = 'role_admin';
+    public const EDITOR = 'role_editor';
+    public const USER = 'role_user';
+}
