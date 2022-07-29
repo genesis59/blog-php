f6f8239dd8b58d11916e8728995d4b8bd9f8f4f1
Greg Dev <gvandevray@numericable.fr>
2022-07-29 10:54:28 +0200

    fix name role_utilisateurs to role_users

--- sql/fixtures.sql
+++ sql/fixtures.sql
@@ -1,6 +1,6 @@
 USE blog_php;
 
-INSERT INTO user(pseudo, role_utilisateur, email, password, createdAt, updatedAt)
+INSERT INTO user(pseudo, role_users, email, password, createdAt, updatedAt)
 VALUES ('toto', 'role_admin', 'admin@admin.com', 'test', ADDDATE(NOW(), INTERVAL -4 DAY), NOW()),
        ('tata', 'role_editor', 'editor@editor.com', 'test', ADDDATE(NOW(), INTERVAL -3 DAY), NOW()),
        ('titi', 'role_user', 'user@user.com', 'test', ADDDATE(NOW(), INTERVAL -2 DAY), NOW()),
