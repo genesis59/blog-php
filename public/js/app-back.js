c024bf7034c046846f2bdf47a0ec77bbb5c926bf
Greg Dev <gvandevray@numericable.fr>
2022-09-20 21:53:44 +0200

    create page to manage comments into backoffice

---
+++ public/js/app-back.js
@@ -0,0 +1,28 @@
+const modalContainer = document.querySelector(".modal-container");
+const modalTriggers = document.querySelectorAll(".modal-trigger");
+const modalTitleText = document?.getElementById("modal-title-text");
+const modalQuestionText = document?.getElementById("modal-question-text");
+const buttonSubmit = document?.getElementById("button-submit");
+
+
+modalTriggers.forEach(trigger => trigger.addEventListener("click",toggleModal));
+buttonSubmit?.addEventListener("click",launchForm)
+
+function toggleModal(e){
+    e.preventDefault();
+    if(e.target.name === "accept"){
+        modalTitleText.innerText = "Validation";
+        modalQuestionText.innerText = "Voulez-vous vraiment accepter ce commentaire?";
+    }
+    if(e.target.name === "delete"){
+        modalTitleText.innerText = "Suppression";
+        modalQuestionText.innerText = "Voulez-vous vraiment supprimer ce commentaire?";
+    }
+    buttonSubmit.name = e.target.name;
+    buttonSubmit.value = e.target.value;
+    modalContainer.classList.toggle("active")
+}
+
+function launchForm(){
+    document.forms["formComment"].submit()
+}
